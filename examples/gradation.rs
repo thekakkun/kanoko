114b0f7da92bb96e7a691eefbd75003e220226a1
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2025-12-21 20:17:51 -0500

    New example to illustrate generating shapes based on index

---
+++ examples/gradation.rs
@@ -0,0 +1,32 @@
+use kanoko::{
+    Canvas, Color,
+    point_set::lattice::{Index, Lattice},
+    shape::Polygon,
+};
+
+/// An example with minimal randomization, based on the traditional Japanese tie-dye pattern
+fn main() {
+    let background_color = "#ddd".try_into().unwrap();
+    let mut canvas_builder = Canvas::builder()
+        .size(2560.0, 1440.0)
+        .background_color(background_color)
+        .points(
+            Lattice::rectangular_builder()
+                .grid_size(7, 5)
+                .len_a(300.0)
+                .len_b(200.0)
+                .build(),
+        );
+
+    canvas_builder.add_shape(
+        Polygon::builder()
+            .sides_fn(|Index { u, .. }| (*u as u8) + 3)
+            .size_fn(|Index { v, .. }| 70.0 + (v + 1) as f64 * 30.0)
+            .color_fn(|Index { u, v }| Color::new((*u + 1) as u8 * 25, 0, (*v + 1) as u8 * 20, 255))
+            .build(),
+    );
+
+    let canvas = canvas_builder.build();
+    let document = canvas.render(|_| true);
+    svg::save("examples/gradation.svg", &document).unwrap();
+}
